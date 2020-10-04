fbc476259dc72a5bc82995a3b783d9ea81e3c57b
windkiss5 <128541916@qq.com>
2020-10-04 21:13:28 +0800

    新增SVM

---
+++ SupportVectorMachine/SVM.py
@@ -0,0 +1,6 @@
+# Python
+# -*- coding: UTF-8 -*-
+# @Time: 2020/10/4 21:12
+# @Author: WINDKISS
+# @File: SVM.py
+# @Function: What SVM.py can do?
--- SupportVectorMachine/test.py
+++
@@ -1,6 +0,0 @@
-# Python
-# -*- coding: UTF-8 -*-
-# @Time: 2020/9/13 16:26
-# @Author: WINDKISS
-# @File: test.py
-# @Function: What test.py can do? 
